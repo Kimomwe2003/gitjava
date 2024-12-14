23016ee0b35d754edbc95137848974b96005f087
Kimomwe2003 <lidyakimomwe@gmail.com>
2024-12-14 20:24:35 +0300

    myn first commit

---
+++ Approve.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Approve{
+	public static void main(String[]args){
+		Scanner m = new Scanner(System.in);
+	    System.out.println("please enter annual income");
+		int annualincome = m.nextInt();
+		System.out.println("please enter credit score");
+		int creditscore = m.nextInt();
+		System.out.println("please enter loan amount");
+		int loanamount = m.nextInt();
+		if(annualincome==$10000 && creditscore>=500 && loan<=(4*annualincome){
+			System.out.println("approve for a loan");
+		}else{
+			System.out.println("no approve for a loan");
+			
+		}
+		
+			
+	}
+}
+			
---
+++ Renting.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Renting{
+	public static void main(String[]args){
+		Scanner m = new Scanner(System.in);
+		System.out.println("please enter days");
+		int days = m.nextInt();
+		System.out.println("please enter type of car");
+		String car = m.nextLine();
+		double totalcost = number of days * type of car;
+		if(rentalperiod>3){
+			double totalcost = totalcost*0.05;
+			System.out.println("the total cost is:" +totalcost);
+			
+		}else{
+			System.out.println("the total cost is:" +totalcost);
+		}
+		
+		
+		
+	}
+}
